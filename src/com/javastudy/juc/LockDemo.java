e5c34bc6e9131455a1999f8d5a2c7fe5714d10bb
Simon <lay3399@outlook.com>
2019-11-20 22:25:44 +0800

    hello git

--- src/com/javastudy/juc/LockDemo.java
+++ src/com/javastudy/juc/LockDemo.java
@@ -1,9 +1,39 @@
 package com.javastudy.juc;
 
-public class LockDemo {
-    public static void main(String[] args) {
-        System.out.println("hello ");
-        System.out.println("hello lxm");
 
-    }
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 线程5种状态：创建、阻塞、就绪、运行、死亡
+ */
+public class LockDemo {
+            public static void main(String[] args) {
+                        Ticket ticket = new Ticket();
+
+                        new Thread(() -> {for (int i = 1; i <= 40; i++)ticket.sale(); }  , "A").start();//线程A就绪
+                        new Thread(() -> {for (int i = 1; i <= 40; i++)ticket.sale(); }  , "B").start();//线程B就绪
+                        new Thread(() -> {for (int i = 1; i <= 40; i++)ticket.sale(); }  , "C").start();//线程C就绪
+
+            }
+}
+
+class Ticket {
+            private int number = 30;
+            Lock lock = new ReentrantLock();
+
+            public void sale() {
+                        lock.lock();
+                        try {
+                                    if (number > 0) {
+                                                System.out.println(Thread.currentThread().getName() + "\t卖出第：" + (number--) + "\t 还剩下： " + number);
+                                    }
+                        } catch (Exception e) {
+                                    e.printStackTrace();
+                        } finally {
+                                    lock.unlock();
+                        }
+            }
+
+
 }
