0eac729d0941da70353fd2f33f5fe24e73348b98
lay3399 <lay3399@outlook.com>
2019-11-20 20:21:04 +0800

    hello git

---
+++ src/com/javastudy/juc/LockDemo.java
@@ -0,0 +1,9 @@
+package com.javastudy.juc;
+
+public class LockDemo {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+        System.out.println("hello lxm");
+
+    }
+}
